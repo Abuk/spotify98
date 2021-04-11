cde8443c555e6c8361e6a64119498a28607336e9
Jakub Schenk <abukcz@gmail.com>
2021-04-11 22:12:01 +0200

    client: fix padding of app
    welcome win95 green wallpaper lmao

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Player from "./components/player/Player";
 import Login from "./components/login/Login";
 import { getLoginState, logout } from "./components/login/loginHelper";
 import "98.css";
+import "./App.css";
 
 function App() {
   const [userLoginState, setUserLoginState] = useState(false);
@@ -19,36 +20,34 @@ function App() {
   }, [handleUserLoginState]);
 
   return (
-    <div className="h-full min-h-full w-100">
-      <div className="window h-screen">
-        <div className="title-bar">
-          <div class="title-bar-text">play98</div>
-          <div class="title-bar-controls">
-            <button aria-label="Minimize"></button>
-            <button aria-label="Maximize"></button>
-            <button aria-label="Close"></button>
-          </div>
+    <div className="window my-2 mx-2 h-custom w-custom">
+      <div className="title-bar ">
+        <div className="title-bar-text">play98</div>
+        <div className="title-bar-controls">
+          <button aria-label="Minimize"></button>
+          <button aria-label="Maximize"></button>
+          <button aria-label="Close"></button>
         </div>
-        <div className="window-body min-w-screen">
-          {getLoginState() ? (
-            <div className="">
-              <button
-                onClick={() => {
-                  setTimeout(200);
-                  handleUserLoginState(false);
-                  logout();
-                }}
-              >
-                logout
-              </button>
-              <div className="absolute inset-x-0 bottom-0">
-                <Player></Player>
-              </div>
+      </div>
+      <div className="window-body my-0 mx-0">
+        {getLoginState() ? (
+          <div className="">
+            <button
+              onClick={() => {
+                setTimeout(200);
+                handleUserLoginState(false);
+                logout();
+              }}
+            >
+              logout
+            </button>
+            <div className="absolute inset-x-0 bottom-0 my-2 mx-2">
+              <Player></Player>
             </div>
-          ) : (
-            <Login></Login>
-          )}
-        </div>
+          </div>
+        ) : (
+          <Login></Login>
+        )}
       </div>
     </div>
   );
